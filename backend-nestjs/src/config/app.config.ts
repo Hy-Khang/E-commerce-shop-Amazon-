269d0c111bde1022a5fde244f3448052d1fd8db5
Hy-Khang <khangngodev@gmail.com>
2026-06-13 01:30:40 +0700

    fix some bugs

--- backend-nestjs/src/config/app.config.ts
+++ backend-nestjs/src/config/app.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 import { resolve, join } from 'path';
 
 export default registerAs('app', () => ({
-  port: parseInt(process.env.APP_PORT ?? '3000', 10),
+  port: parseInt(process.env.APP_PORT ?? process.env.PORT ?? '3000', 10),
   prefix: process.env.APP_PREFIX || 'api/v1',
   corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:5173',
   nodeEnv: process.env.NODE_ENV || 'development',
